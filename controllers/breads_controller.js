76a5def0cfe79a1f571336bd949b216de72302e0
Aaron Canales <aaroncanales0107@gmail.com>
2023-04-17 19:03:53 -0700

    first commit

---
+++ controllers/breads_controller.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const breads = express.Router();
+const bread = require('../models/breads.js')
+
+//index
+breads.get('/', (req, res) => {
+    res.send(bread)
+})
+
+//show
+breads.get('/:arrayIndex', (req, res) => {
+    res.send(bread[req.params.arrayIndex])
+})
+
+module.exports = breads
